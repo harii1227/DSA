e4c3c289c2a248be8c95f5bcdc8a1e3300cb7308
Hari Narayan Singh <rs22201227@gmail.com>
2024-02-27 21:53:55 +0530

    pointer

---
+++ Pointer/Second.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // int a = 5;
+    // int* p = &a;
+    // p = p+1;
+    // cout<<*p;
+
+    // int arr[5]={12,23,34,56,67};
+    // arr = arr +1;
+
+    char ch[10]='a';
+    char *cptr = &ch[0];
+    cout<<cptr;
+}
---
+++ Pointer/first.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+/**
+ *     int a = 5;
+    cout<<a<<endl;
+    cout<<"adress of s"<<&a<<endl;
+
+
+    //pointer creation
+    int* ptr =&a;
+
+    cout<<"accessing: "<< *ptr<<endl;
+
+    return 0;
+*/
+
+
+    // int a =5;
+    // int* ptr = &a;
+    // cout<<sizeof(*ptr)<<endl;
+
+    // char ch = 'a';
+    // char* cptr = &ch;
+    // cout<<sizeof(*cptr)<<endl;
+
+
+    // long lachi=10;
+    // long* lptr =&lachi;
+    // cout<<sizeof(*lptr)<<endl;
+
+
+
+
+    int a =100;
+    int* ptr = &a;
+    a=+1;
+    ptr=ptr+1;
+    cout<<a<<endl;
+    cout<<ptr<<endl; 
+
+
+}
