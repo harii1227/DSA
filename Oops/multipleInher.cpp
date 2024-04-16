96289d0b28bd4ca6c433b9ac2249d24e634763a6
Hari Narayan Singh <rs22201227@gmail.com>
2024-04-16 14:12:29 +0530

    dsa

---
+++ Oops/multipleInher.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+
+    public:
+    int age;
+    int weight;
+
+
+    public:
+    void bark(){
+        cout<<"speaking"<<endl;
+    }
+};
+class Human{
+
+    public:
+    string color;
+
+    public:
+    void speak(){
+        cout<<"speaking"<<endl;
+    }
+
+};
+class Hybrid: public Animal,public Human{
+
+};
+
+int main(){
+    Hybrid obj1;
+    obj1.speak();
+    obj1.bark();
+    cout<<obj1.age<<endl;
+
+    return 0;
+}
