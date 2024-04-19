74a35d31cee9077f4c17ac441a831e0ccdee12c6
Hari Narayan Singh <rs22201227@gmail.com>
2024-04-20 00:01:55 +0530

    dsa

---
+++ Linkedlist/1St.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+
+    int data;
+    node *next;
+};
+
+int main(){
+
+   node *head=new node();
+
+   int *ptr=new int;  
+
+    return 0;
+}
